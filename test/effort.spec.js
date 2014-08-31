813f0b3552fe1fa94b971bb37c93083781fad740
Nuno Rosa <nuno.sp.rosa@gmail.com>
2014-08-31 14:24:08 +0100

    distance and time calculations between efforts

--- test/effort.spec.js
+++ test/effort.spec.js
@@ -9,6 +9,13 @@ describe('Effort', function(){
                                       params.units,
                                       params.distance,
                                       params.sample_period);
+
+  var pacemakerB = new Effort.fromPace(6,
+                                      params.units,
+                                      params.distance,
+                                      params.sample_period);
+
+  var ten_minutes = 10*60;
   var fifty_minutes = 50*60;
   var one_hour = 3600;
 
@@ -29,6 +36,39 @@ describe('Effort', function(){
   describe('atDistance', function(){
     it('should have 50 minutes at 10000m', function(){
       expect(pacemaker.atDistance(10000).time).to.equal(fifty_minutes);
+    });
+  });
+
+  describe('distanceTo', function(){
+    it('should be 333 meters apart at 10 minutes', function(){
+      var distanceToB = pacemaker.distanceTo(pacemakerB, ten_minutes);
+      var distanceToA = pacemakerB.distanceTo(pacemaker, ten_minutes);
+
+      expect(distanceToB).to.equal(333);
+      expect(distanceToA).to.equal(333);
+      expect(pacemaker.distanceTo(pacemaker, ten_minutes)).to.equal(0);
+      expect(pacemakerB.distanceTo(pacemakerB, ten_minutes)).to.equal(0);
+    })
+  });
+
+  describe('timeTo', function(){
+    it('should be 36 seconds apart at 10 minutes', function(){
+      var timeToB = pacemaker.timeTo(pacemakerB, ten_minutes);
+      var timeToA = pacemakerB.timeTo(pacemaker, ten_minutes);
+
+      expect(timeToB).to.equal(36);
+      expect(timeToA).to.equal(36);
+      expect(pacemaker.timeTo(pacemaker, ten_minutes)).to.equal(0);
+      expect(pacemakerB.timeTo(pacemakerB, ten_minutes)).to.equal(0);
+    })
+  });
+
+  describe('aheadOf', function(){
+    it('should be ahead at 10 minutes', function(){
+      expect(pacemaker.aheadOf(pacemakerB, ten_minutes)).to.be.true;
+      expect(pacemakerB.aheadOf(pacemaker, ten_minutes)).to.be.false;
+      expect(pacemaker.aheadOf(pacemaker, ten_minutes)).to.be.false;
+      expect(pacemakerB.aheadOf(pacemakerB, ten_minutes)).to.be.false;
     });
   });
 
